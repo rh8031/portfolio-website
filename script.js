bcdbf266593e83a9af807788975110078b0015b4
rh8031 <rhassan6920@gmail.com>
2026-06-11 04:07:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 document.getElementById("contactBtn").addEventListener("click", function () {
     alert("rakib.hassan80@qmail.cuny.edu");
 });
+
+document.getElementById("confettiBtn").addEventListener("click", function() {
+
+    for (let i = 0; i < 50; i++) {
+        let confetti = document.createElement("div");
+
+        confetti.innerHTML = "🎉";
+        confetti.style.position = "fixed";
+        confetti.style.left = Math.random() * window.innerWidth + "px";
+        confetti.style.top = Math.random() * window.innerHeight + "px";
+        confetti.style.fontSize = "24px";
+
+        document.body.appendChild(confetti);
+
+        setTimeout(() => {
+            confetti.remove();
+        }, 2000);
+    }
+
+});
