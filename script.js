fea3dfef3bf998a52a386eae109e58d19f6b4be2
rh8031 <rhassan6920@gmail.com>
2026-06-11 04:16:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+// Display contact information when the Contact button is clicked
 document.getElementById("contactBtn").addEventListener("click", function () {
     alert("rakib.hassan80@qmail.cuny.edu");
 });
 
+// Create a simple confetti effect when the Celebrate button is clicked
 document.getElementById("confettiBtn").addEventListener("click", function() {
 
+    // Generate 50 confetti emojis
     for (let i = 0; i < 50; i++) {
         let confetti = document.createElement("div");
 
@@ -13,8 +16,10 @@ document.getElementById("confettiBtn").addEventListener("click", function() {
         confetti.style.top = Math.random() * window.innerHeight + "px";
         confetti.style.fontSize = "24px";
 
+        // Add confetti to the page
         document.body.appendChild(confetti);
 
+        // Remove confetti after 2 seconds
         setTimeout(() => {
             confetti.remove();
         }, 2000);
